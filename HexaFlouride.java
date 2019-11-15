455424f01ebdcd682e15ecca2e957cf6161e12c6
RohanRathodRajput <RohanRathodRajput@gmail.com>
2019-11-15 22:49:26 +0530

    adding Xenon Hexaflouride files

---
+++ HexaFlouride.java
@@ -0,0 +1,8 @@
+class HexaFlouride{
+public static void main(String[] args){
+System.out.println("HexaFlouride Xlass Output") ;
+
+
+}
+
+}
---
+++ Xenon.java
@@ -0,0 +1,8 @@
+class Xenon{
+public static void main(String[] args){
+System.out.println("Xenon Xlass Output") ;
+
+
+}
+
+}
